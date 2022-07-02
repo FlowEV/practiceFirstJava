f64598ededb309c9da73c5dbeb5453f6db93b1eb
FlowEV <murpurpower@gmail.com>
2022-07-02 20:12:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String helloText = "hello";
+        String javaText = " java";
+        System.out.println(helloText.length() + "+" + javaText.length());
+
+    }
+}
